bb41b93a88131d3010e5ef1eae8b52cf9abf4201
GRINI Simen <simen.grini@soprasteria.com>
2019-12-12 14:05:52 +0100

    Clean up

--- crawl3.py
+++ crawl3.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import requests
-import queue
+import queue  # queue.Empty exception
 import logging
 import os
 from bs4 import BeautifulSoup
@@ -10,7 +10,6 @@ import time
 
 class Scraper:
     def __init__(self, base_url):
-        #base_url = 'http://quotes.toscrape.com/'
         manager = multiprocessing.Manager()
         self.base_url = base_url
         self.processed_urls = manager.list()
@@ -20,6 +19,8 @@ class Scraper:
         self.url_queue = multiprocessing.Queue()
         self.logger = self.create_logger()
         self.initialize()
+        self.max_requests_per_second = 30
+        self.num_threads = 10
 
 
     def initialize(self):
@@ -36,24 +37,23 @@ class Scraper:
         return logger  
     
     def scrape(self, url_queue):
-        #logger = multiprocessing.get_logger()
-        print("THREAD")
-        self.logger.info("QUEUELENGTH:  " + str(url_queue.qsize()))
         proc = os.getpid()
-        #print(url_queue.get())
-        
-        while not url_queue.empty():
-            #print("QUEUELENGTH:  " + str(url_queue.qsize()))
+        print("Thread: " + str(proc) +" started")
+        proc = os.getpid()
+        while True:
+            time.sleep(self.num_threads / self.max_requests_per_second)
             try:
-                url = url_queue.get()   
-                while (url in self.processed_urls):
-                    url = url_queue.get()
-                self.logger.info(str(proc) + ", url: " + url)  
-                self.find_links(url)
-            except:
-                print("Could not get from queue")
-                return
-        print("threadStopped")
+                url = url_queue.get_nowait()
+                while url in self.processed_urls:
+                    url = url_queue.get_nowait()
+            except queue.Empty:
+                time.sleep(0.5)
+                if url_queue.empty():
+                    break
+                else:
+                    continue
+            self.find_links(url)
+        print("Thread: " + str(proc) + " Stopped")
         
 
     def find_links(self, url):
@@ -63,13 +63,9 @@ class Scraper:
             self.add_elem_if_not_in_list(self.broken_urls, url)
             self.add_elem_if_not_in_list(self.processed_urls, url)
             return
-        #print(res.text)
         links = self.get_all_links_on_page(res.text)
         self.filter_links(links)
-        #print(links)
-        proc = os.getpid()
         self.add_elem_if_not_in_list(self.processed_urls, url)
-        #print(self.processed_urls)
 
 
     def add_elem_if_not_in_list(self, lst, elem):
@@ -82,7 +78,6 @@ class Scraper:
         for link in soup.find_all('a'):
             if link.has_attr('href'):
                 links.append(link.get("href"))
-        # print("LLL:  " + links[0])
         return links
         
     def filter_links(self, links):
@@ -97,58 +92,31 @@ class Scraper:
                 self.add_elem_if_not_in_list(self.foreign_urls, link_str)
         for elem in new_local_links:
             if elem not in self.processed_urls:
-                proc = os.getpid()
-                # print("#####################################")
-                # print(str(proc) + "   " + elem)
-                # print(str(proc) + "   " + str(self.processed_urls))
-                # print("#####################################")
                 self.url_queue.put(elem)
             if elem not in self.local_urls:
                 self.local_urls.append(elem)
 
     def start_worker_threads(self):
-        
-        print("Start")
         processes = []
-        #print("HEI:  " + self.url_queue)
-        for n in range(10):
+        for n in range(self.num_threads):
             p = multiprocessing.Process(target=self.scrape, args=(self.url_queue,))
-            # p.daemon = True
             processes.append(p)
             p.start()
         try:
-            
             for p in processes:
-                
                 p.join()
                 p.terminate()
         except KeyboardInterrupt:
             for p in processes:
                 p.terminate()
                 p.join()
-        # p = Pool(10)
-        # p.map(self.scrape, self.url_queue) 
-        # p.terminate()
-        # p.join()
 
-
-# scraper = Scraper('http://quotes.toscrape.com')
-# print(scraper.unvisited_local_urls)
-# scraper.start_worker_threads()
-# # print(scraper.all_urls)
-# print(scraper.foreign_urls)
-# print(scraper.processed_urls)
-# print(scraper.unvisited_local_urls)
-# print(scraper.broken_urls)
 
 
 
 
 if __name__ == '__main__':    
     scraper = Scraper('http://quotes.toscrape.com')
-    #scraper.runInParallel(numProcesses=2, numThreads=4)
-    
-    #print(scraper.unvisited_local_urls)
     print("HEI")
     t0 = time.time()
     scraper.start_worker_threads()
@@ -159,9 +127,3 @@ if __name__ == '__main__':
     print("local: " + str(scraper.local_urls)+ "    length: " + str(len(scraper.local_urls)))
     print("processed: " + str(scraper.processed_urls) + "    length: " + str(len(scraper.processed_urls)))
     print("TIME: " + str(t1-t0))
-    # print(scraper.all_urls)
-# initialize()
-# start_worker_threads()
-# print(broken_urls)
-# print(unvisited_local_urls)
-# print(visited_urls)
